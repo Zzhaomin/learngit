da493af9a4b8b0ea5928f8c2a044d784c6735711
zhaoming <653355693@qq.com>
2018-09-12 20:43:35 +0800

    myFirstVe

---
+++ wcHomework.java
@@ -0,0 +1,115 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class wcHomework {
+    private static int cCount = 0;
+    private static int wCount = 0;
+    private static int lCount = 0;
+    private static int codeLineCount = 0;
+    private static int nullLineCount = 0;
+    private static int noteLineCount = 0;
+    private static String command = "";
+    private static String[] string;
+    private static String string1;
+    private static String File;
+    public static int cCount() throws IOException {
+    	 cCount = 0;
+    	 File file = new File(File);
+    	 FileInputStream fis = new FileInputStream(file);
+         InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+         BufferedReader br = new BufferedReader(isr);
+   //      StringBuffer sb = new StringBuffer();
+         String line = "";
+         while ((line = br.readLine()) != null) {
+        	 cCount  += line.length();
+         }
+         return cCount; 
+    }
+    
+    public static int wCount() throws IOException {
+    	 wCount = 0;
+    	File file = new File(File);
+   	    FileInputStream fis = new FileInputStream(file);
+        InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+        BufferedReader br = new BufferedReader(isr);
+        StringBuffer sb = new StringBuffer();
+        String line = "";
+        while ((line = br.readLine()) != null) {
+        	sb.append(line);
+        	line = line.trim();//trim()方法返回调用字符串对象的一个副本，但是所有起始和结尾的空格都被删除了
+            wCount = sb.toString().split("\\s+").length;
+        }
+        return wCount;
+    }
+    
+    public static int lCount() throws IOException {
+        lCount = 0;
+    	File file = new File(File);
+   	    FileInputStream fis = new FileInputStream(file);
+        InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+        BufferedReader br = new BufferedReader(isr);
+        StringBuffer sb = new StringBuffer();
+        String line = "";
+        while ((line = br.readLine())!= null) {
+        	lCount = lCount +1;
+        }
+        return lCount;
+    }
+    public static void moreLineCount() throws IOException {
+        codeLineCount = 0;
+        nullLineCount = 0;
+        noteLineCount = 0;
+    	File file = new File(File);
+   	    FileInputStream fis = new FileInputStream(file);
+        InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+        BufferedReader br = new BufferedReader(isr);
+        StringBuffer sb = new StringBuffer();
+        String line = "";
+        while ((line = br.readLine()) != null) {
+             if (line == "" || line.length() <= 1) {
+                 nullLineCount =  nullLineCount + 1;
+                 continue;
+             }
+             int a = line.indexOf("/");//返回第一次出现的指定子字符串在此字符串中的索引。
+             int b = line.substring(a + 1).indexOf("/");
+             if (b == 0) {
+                 noteLineCount++;
+                 continue;
+             }            
+             codeLineCount++;
+        }
+    }
+	public static void main(String[] args) throws IOException {
+		// TODO 自动生成的方法存根
+		System.out.print("wc.exe ");
+        Scanner s = new Scanner(System.in);
+        command = s.nextLine();
+        string = command.split(" ");
+        string1 = string[0];
+        File = string[1];
+        
+        switch(string1) {
+        case "-c" :
+        	System.out.println("字符数 ：" + cCount());
+        	break;
+        case "-w" :
+        	System.out.println("词的数目 ：" + wCount());
+        	break;
+        case "-l" :
+        	System.out.println("行数 ： " + lCount());
+        	break;
+        case "-a" :
+        	moreLineCount();
+        	System.out.println("代码行数 ： " + codeLineCount);
+        	System.out.println("空白行数 ： " + nullLineCount);
+        	System.out.println("注释行数 ： " + noteLineCount);
+        	break;
+        }
+	}
+
+}
